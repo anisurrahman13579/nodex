d4274d0f4fd01586d9238706e38793a0d69fec75
md:Anisur rahman <anisuranisur775@gmail.com>
2024-11-12 00:26:30 +0600

    Add files via upload

---
+++ class-04/script-04.js
@@ -0,0 +1,130 @@
+
+// array er modhe  duita jish khub dokar 
+// 1. index.
+// 2. value.
+/*
+let a = [4,3,5];
+//console.log(a);
+console.log(a[1]);
+console.log(a[0]);
+console.log(a[2]);*/
+
+
+//let arr= [10,20,30,40,50,60,70,80,90];
+// console.log(x);
+//   console.log(x[0]);
+//   console.log(x[1]);
+//   console.log(x[2]);
+//   console.log(x[3]);
+//   console.log(x[4]);
+//   console.log(x[5]);
+//   console.log(x[6]);
+//   console.log(x[7]);
+//   console.log(x[8]);
+//   console.log([9]);
+// for(let i=0;i<=9;i++)
+// {
+//    console.log(arr[i]);
+// }
+
+
+
+
+// index = 0.
+// length = 1.
+
+/*for(i=0;i<arr.length;i++)
+{
+    console.log("index: " +i+ "-> "+ "value"+ arr[i]);
+}*/
+
+
+// let max = [6,"hell0",4.3,"basify"];
+// for(let i = 0; i<max.length; i++)
+// {
+//     console.log(max[i]);
+// }
+       
+
+    // let ban = [[12,23,34],[43,53,57]];
+    //    for(let i = 0; i<2;i++)
+    //     {
+    //          for(let j = 0; j<3;j++)
+    //          {
+    //             // console.log(ban[i][j]);
+                
+    //          }
+    //     }
+        
+
+
+
+    //    let numbers = [6,4,8,6];
+     //    for(let i = 0; i<numbers;i++)
+     //    {
+     //     console.log(numbers[i]);
+     //    }
+
+    //    for(let i of numbers)
+      //    {
+     //     console.log(i);
+     //    }
+
+         
+
+    //      for(let value of numbers)
+    //      {
+    //         console.log(value);
+    //      }
+           
+
+
+
+
+/*
+           let obj = {
+            x:1,
+            y:2,
+            z:3,
+            name: "anisur rahman",
+            upcoming : "new web app"
+           }
+           for(let key in obj)
+           {
+              //  console.log(key);
+             // console.log(obj[key]);
+               console.log(key + ": " + obj[key]);
+           }
+               */
+            //    let car = {
+            //     brandName: "BMW",
+            //     color: "Black",
+            //     trie: 4
+            //    }
+            //    let bus = {
+            //     brandName: "toyota",
+            //     color: "silvar",
+            //     trie: 4
+            //    }
+            //    for(let key in car)
+            //    {
+            //      console.log(key+": "+car[key])
+            //    }
+                
+
+
+
+
+            //    let test = [[10,20],[30,40]];
+            //   // console.log(test[1][0])
+            //      for(let i = 0 ; i< 2 ; i++)
+            //      {
+            //         for (let j = 0; j<3 ; j++)
+            //         {
+            //             console.log(test[i][j])
+            //         }
+            //      }
+
+                 
+
+        
